a3f4dc97ad77a2d8694e2f581fe5da1f498e5d6e
Artem Revenko <artreven@gmail.com>
2013-06-07 16:58:37 +0200

    Add possibility to read context with object names

--- fca/readwrite/tab_separated.py
+++ fca/readwrite/tab_separated.py
@@ -4,6 +4,48 @@
 import csv
 import fca
 
+def read_txt_with_names(path):
+    """Read context from path, which is tab separated txt file
+
+    Format
+    ======
+
+    First line is tab separated attributes' names
+    Next an empty line
+    The first value in every line is on object name.
+    Then tab separated 1 and 0, each line corresponds to one object.
+
+    """
+    input_file = open(path, "rb")
+    rdr = csv.reader(input_file, delimiter="\t")
+    rec = rdr.next()                            # read attributes names
+
+    attributes = []
+    for attr in rec:
+        attributes.append(str(attr).strip())
+    
+    rdr.next()                                  # empty line
+    
+    table = []
+    objects = []
+    for rec in rdr:
+        objects.append(rec[0])                  # read objects names
+        line = []
+        for num in rec:
+            if num == "0":
+                line.append(False)
+            elif num == "1":
+                line.append(True)
+        table.append(line)
+    input_file.close()
+
+    if len(attributes) != len(table[0]):
+        input_file = open(path, "rb")
+        attributes = input_file.readline().split("\t")[:-1]
+        input_file.close()
+
+    return fca.Context(table, objects, attributes)
+
 def read_txt(path):
     """Read context from path, which is tab separated txt file
 
