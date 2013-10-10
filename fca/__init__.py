ad064de94452ddf609fc288941d687ad0187e13a
Artem Revenko <artreven@gmail.com>
2013-10-10 15:13:43 +0200

    Add read_txt_with_names to __init__.py of fca package

--- fca/__init__.py
+++ fca/__init__.py
@@ -13,7 +13,7 @@ from fca.algorithms import (norris, compute_covering_relation,
                             scale_mvcontext, compute_dg_basis)
 from fca.readwrite import (read_txt, read_cxt, write_cxt, write_dot,
                            read_mv_txt, read_xml, write_xml, write_mv_txt,
-                           uread_cxt, uwrite_cxt)
+                           uread_cxt, uwrite_cxt, read_txt_with_names)
 from fca.algorithms.filtering import (filter_concepts, compute_estability,
 compute_istability, compute_separation_index, 
 compute_probability, compute_index)
