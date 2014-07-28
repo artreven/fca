a9cab0569be52595250079f0f3217e46a366b371
Artem Revenko <artreven@gmail.com>
2014-07-28 12:46:51 +0200

    no message

---
+++ fca/tests/test_implication.py
@@ -0,0 +1,17 @@
+'''
+Use with Nosetests (https://nose.readthedocs.org/en/latest/)
+
+Created on May 22, 2014
+
+@author: artreven
+'''
+import fca
+
+def test_unit_implication():
+    imp = fca.Implication(set((1,2,3)), set((4,)))
+    uimp = fca.UnitImplication(imp.premise, imp.conclusion.pop())
+    assert uimp == imp
+        
+def test_unit_conclusion():
+    uimp = fca.UnitImplication(set((1,2,3)), set((4,5)))
+    assert uimp.conclusion is not set
