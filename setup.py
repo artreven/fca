4be11370986436dd342c793e22fb7f0fcab7b90d
Artem Revenko <artreven@gmail.com>
2019-06-27 12:14:30 +0200

    Change setup procedure to using pbr

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-from distutils.core import setup
+#!/usr/bin/env python
 
-setup(name             = "fca",
-      version          = "0.0.2",
-      author           = ["Sergei Obiedkov", "Nikita Romashkin", "Artem Revenko"],
-      author_email     = ["not given", "romashkin.nikita@gmail.com", "artreven@gmail.com"],
-      description      = "Python package for formal concept analysis",
-      keywords         = ["Formal Concept Analysis", "Data Analysis",
-                          "Computer Science", "Algebraic Lattices",
-                          "Implications", "Artificial Intelligence"],
-      license          = "LGPL",
-      platforms        = ["Linux", "Mac OS X", "Windows XP/2000/NT"],
-      requires=['numpy', 'numpy']
-      )
+from setuptools import setup
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
